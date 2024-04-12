029c3899b8401b6310dcf6bb2b1f3476f1c63cb5
Mert <122644315+mertkavilcioglu@users.noreply.github.com>
2024-04-12 13:38:45 +0300

    Revert "Merge branch 'EnemySpawner' into ConflictTestForMergeBranch"
    This reverts commit 6941ac878df7be892c76c85316eaec133a8d6c17, reversing
    changes made to f47fd5d9e4b9ddee50dd9666e22001daa3071b4c.

--- Scripts/rigid_body_2d.gd
+++ Scripts/rigid_body_2d.gd
@@ -1,16 +1,26 @@
+class_name Enemy #NEW ********************************
 extends RigidBody2D
 
-@onready var player = $"../Player"
+signal hit() #NEW ********************************
+var enemyHealth:int = 3 #NEW ********************************
+var playerDamage:int = 1 #NEW ********************************
+
+@onready var player = $"../Character"
 @export var enemyMaxSpeed: float = 100.0
-#@export var enemyAcc: float = 1
 @export var radius = 150
 
 func _physics_process(delta):
 	var enemyMotion = Vector2()
 
-	enemyMotion.x =  (player.position.x + radius) - self.position.x
-	enemyMotion.y =  (player.position.y + radius) - self.position.y
-	#print(player.position.x)
+	enemyMotion.x = (player.position.x + radius) - self.position.x
+	enemyMotion.y = (player.position.y + radius) - self.position.y
 
 	linear_velocity.x = enemyMaxSpeed * enemyMotion.x * delta
 	linear_velocity.y = enemyMaxSpeed * enemyMotion.y * delta
+
+func getHit(): #NEW ********************************
+	emit_signal("hit") #NEW ********************************
+	if enemyHealth > 1: #NEW ********************************
+		enemyHealth -= 1 #NEW ********************************
+	elif enemyHealth <= 1: #NEW ********************************
+		queue_free() #NEW ********************************
